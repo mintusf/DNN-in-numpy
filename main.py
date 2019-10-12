032c4eee416eb89cf3166ed6a6eeb6c76f3d82b9
Filip M <40537332+mintusf@users.noreply.github.com>
2019-10-12 19:25:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,36 @@ if __name__ == '__main__':
                                 range(y_train.shape[0])])
     y_test_formodel = np.array([y_test[i] == labels for i in
                                range(y_test.shape[0])])
-    model = NN.NNet(
+    
+    """
+    Model is an instance of the NN class. Hyperparameters:
+    -> layers_size - vector containing numbers of nodes in hidden layers 
+        (any number of layers and nodes is possible)
+    -> reg_lambda - a regularization parameter lambda
+    -> activ_func - an activation function used in hidden layers 
+        (in output layer, always sigmoid function is used). 
+        Possible choices are sigmoid, Relu and tanh.
+    -> learning_rate
+    
+    NN.train is a function which trains the model. Hyperparameters are:
+    -> steps_no - number of epochs
+    
+    NN.pred_val is a function which predicts labels for any input and shows random images. 
+    -> im_to_show - number of images to show with predictions
+    """
+    
+    steps_no = 50
+    Model = NN.NNet(
         input_lay_size=X_train.shape[1],
         output_lay_size=y_train_formodel.shape[1],
-        layers_size=[80, 20],
-        reg_lambda=1,
-        activ_func=NN.sigmoid,
+        layers_size=[120,120],
+        reg_lambda=0.1,
+        activ_func=NN.tanh,
         X=X_train,
         y=y_train_formodel,
-        learning_rate=1,
+        learning_rate=0.5,
         )
     (loss_train, loss_test, acc_train, acc_test) = NN.train(
-        model, 100, X_train, y_train, X_test, y_test)
-    NN.pred_val(model, 3, X_test, y_test)
+        Model, steps_no, X_train, y_train, X_test, y_test)
+    im_to_show = 5
+    NN.pred_val(Model, im_to_show, X_test, y_test)
